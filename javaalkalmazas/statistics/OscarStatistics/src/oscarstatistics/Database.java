e1567bede77eeebab0fc37c3f68d6b7e172ab31c
Székely Balázs Csaba <szekelyb91@gmail.com>
2023-05-02 14:41:22 +0200

    statisztika/1. kész

---
+++ javaalkalmazas/statistics/OscarStatistics/src/oscarstatistics/Database.java
@@ -0,0 +1,59 @@
+package oscarstatistics;
+
+import java.sql.*;
+
+public class Database {
+    private String url;
+    private String databaseName;
+    private String user;
+    private String pass;
+    private Connection conn;
+
+    public Database(String url, String databaseName, String user, String pass) {
+        this.url = url;
+        this.databaseName = databaseName;
+        this.user = user;
+        this.pass = pass;
+        this.conn = null;
+        
+        //Driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Baj van! Nincs meg a driver! " +ex);
+        }
+        
+        //kapcsolódás az adatbázishoz
+        try {
+            this.conn = DriverManager.getConnection(this.url+this.databaseName, this.user, this.pass);
+        } catch (SQLException ex) {
+            System.out.println("Az adatbázis vagy az adattábla nem található...");
+        }
+        
+        if(this.conn != null) System.out.println("Sikeres kapcsolódás");
+        
+    }
+    
+    public void showAll(){
+        //Készítsen listát az összes filmrõl a címük és elnyert díjaik számának feltüntetésével!
+        //A listát rendezze a jelölések száma szerint csökkenõ rendbe, ezen belül cím szerint névsorba!
+        if(this.conn != null){
+            String sql = "SELECT cim, dij FROM filmek ORDER BY jelol DESC, cim;";
+            Statement stmt = null;
+            ResultSet rs = null;
+            
+            try { stmt = conn.createStatement();
+            } catch (SQLException ex) { System.out.println("Baj van! Hiba a statement létrehozásában! " + ex); }
+            
+            if(stmt != null){
+                try {    
+                    rs = stmt.executeQuery(sql);
+                    while(rs.next()){
+                        System.out.println(rs.getString("cim")
+                                + " | " + rs.getInt("dij"));
+                    }
+                } catch(SQLException ex){ System.out.println("Baj van! Hiba a query futtatásánál! " + ex); }
+            } 
+        }
+    }
+}
---
+++ javaalkalmazas/statistics/OscarStatistics/src/oscarstatistics/OscarStatistics.java
@@ -0,0 +1,29 @@
+package oscarstatistics;
+
+
+public class OscarStatistics {
+
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/";
+        String databaseName = "oscar";
+        String user = "root";
+        String pass = "";
+        
+        Database db = new Database(url, databaseName, user, pass);
+        
+        
+        System.out.println("1. FELADAT: "); db.showAll();
+        
+        System.out.println("2. FELADAT: ");
+        System.out.println("3. FELADAT: ");
+        System.out.println("4. FELADAT: ");
+        System.out.println("5. FELADAT: ");
+        System.out.println("6. FELADAT: ");
+        System.out.println("7. FELADAT: ");
+        System.out.println("8. FELADAT: ");
+        System.out.println("9. FELADAT: ");
+        System.out.println("10. FELADAT: ");
+    }
+    
+}
